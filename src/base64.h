22549fcf9305cc65cd45bfd0f3fc43f97419855b
Isaac Morneau <isaacmorneau@gmail.com>
2018-09-22 21:58:05 -0700

    removed unused base 64

--- src/base64.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-#include <stdint.h>
-#include <stddef.h>
-
-uint8_t *base64_encode(const uint8_t *src, size_t len, size_t *out_len);
-uint8_t *base64_decode(const uint8_t *src, size_t len, size_t *out_len);
